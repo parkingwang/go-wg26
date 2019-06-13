7d65f98911e1c38459d1d4ddf67db3c59a50aa1b
yoojia.chen <yoojiachen@gmail.com>
2019-06-13 18:37:00 +0800

    添加WG26卡号字面值解析方法；更新其它函数名；

--- wg26.go
+++ wg26.go
@@ -12,6 +12,14 @@ import (
 // Author: 陈哈哈 bitschen@163.com
 //
 
+const (
+	LengthNumber = 10
+)
+
+var (
+	bOrder = binary.BigEndian
+)
+
 // 维根26编码
 type Wg26Id struct {
 	Number    string  // 原始十位卡号
@@ -26,8 +34,8 @@ func (id *Wg26Id) Std() string {
 	return "0" + fmt.Sprintf("%d,%d", id.Wg26Start, id.Wg26End)
 }
 
-// 从原始卡号解析
-func ParseFromSN(number string) *Wg26Id {
+// 从标准10位卡号解析
+func ParseFromCardNumber(number string) *Wg26Id {
 	nInt := TrimZero(number)
 	nHex := fmt.Sprintf("%06X", nInt)
 	bytes, _ := hex.DecodeString(nHex)
@@ -36,26 +44,38 @@ func ParseFromSN(number string) *Wg26Id {
 		Wg26Hex:   nHex,
 		Wg26Bytes: [3]byte{bytes[0], bytes[1], bytes[2]},
 		Wg26Start: uint16(bytes[0]),
-		Wg26End:   binary.BigEndian.Uint16(bytes[1:]),
+		Wg26End:   bOrder.Uint16(bytes[1:]),
 	}
 }
 
 // 从维根26国际标准编码解析
+// [0] Start
+// [1-2] End
 func ParseFromWg26(wg26std [3]byte) *Wg26Id {
-	num := fmt.Sprintf("%d", binary.BigEndian.Uint32([]byte{0, wg26std[0], wg26std[1], wg26std[2]}))
 	return &Wg26Id{
-		Number:    AppendZero(num),
+		Number:    AppendZero(fmt.Sprintf("%d", bOrder.Uint32([]byte{0, wg26std[0], wg26std[1], wg26std[2]})), LengthNumber),
 		Wg26Hex:   fmt.Sprintf("%06X", wg26std),
 		Wg26Bytes: wg26std,
 		Wg26Start: uint16(wg26std[0]),
-		Wg26End:   binary.BigEndian.Uint16(wg26std[1:]),
+		Wg26End:   bOrder.Uint16(wg26std[1:]),
 	}
 }
 
-// 从Uint32类型的维根26国际标准编码解析
-func ParseFromUint32(id uint32) *Wg26Id {
+// 从维根26标准字面卡号解析
+func ParseFromWg26Number(wg26Number string) *Wg26Id {
+	std := fmt.Sprintf("%8s", wg26Number)
+	bytes := make([]byte, 2)
+	bOrder.PutUint16(bytes, toInt(std[3:]))
+	return ParseFromWg26([3]byte{
+		byte(toInt(strings.TrimLeft(std[:3], " "))),
+		bytes[0],
+		bytes[1]})
+}
+
+// 从标准10位卡号的整数卡号解析
+func ParseFromCardNumberValue(number uint32) *Wg26Id {
 	b := make([]byte, 4)
-	binary.BigEndian.PutUint32(b, id)
+	bOrder.PutUint32(b, number)
 	return ParseFromWg26([3]byte{b[1], b[2], b[3]})
 }
 
@@ -65,11 +85,16 @@ func TrimZero(card string) int64 {
 	return v
 }
 
-func AppendZero(card string) string {
-	s := 10 - len(card)
+func AppendZero(txt string, max int) string {
+	s := max - len(txt)
 	zeros := ""
 	for i := 0; i < s; i++ {
 		zeros += "0"
 	}
-	return zeros + card
+	return zeros + txt
+}
+
+func toInt(v string) uint16 {
+	r, _ := strconv.Atoi(v)
+	return uint16(r)
 }
--- wg26_test.go
+++ wg26_test.go
@@ -1,6 +1,7 @@
 package wg26
 
 import (
+	"encoding/binary"
 	"fmt"
 	"testing"
 )
@@ -20,19 +21,39 @@ func TestParseFromSN(t *testing.T) {
 		if "56433B" != fmt.Sprintf("%X", id.Wg26Bytes) {
 			t.Error("Not match")
 		}
+		if 86 != id.Wg26Start {
+			t.Error("not match")
+		}
+		if 17211 != id.Wg26End {
+			t.Error("not match")
+		}
 		if "086,17211" != id.Std() {
 			t.Error("Not match")
 		}
 	}
 
-	testFields(ParseFromSN("0005653307"))
+	testFields(ParseFromCardNumber("0005653307"))
 	testFields(ParseFromWg26([3]byte{0x56, 0x43, 0x3B}))
 }
 
-func TestParse2(t *testing.T) {
-	id := ParseFromUint32(3659533)
+func TestParse1(t *testing.T) {
+	id := ParseFromCardNumberValue(3659533)
 	t.Log("Number: ", id.Number)
 	t.Log("Wg26Hex: ", id.Wg26Hex)
-	t.Log("Wg26Bytes: ", id.Wg26Bytes)
+	t.Logf("Wg26Bytes: %X", id.Wg26Bytes)
+	t.Logf("Wg26Start: %d", id.Wg26Start)
+	t.Logf("Wg26End: %d", id.Wg26End)
+	t.Log("Std: ", id.Std())
+}
+
+func TestParse2(t *testing.T) {
+	data := []byte{0xfb, 0x7c, 0x83, 0x00}
+	wg26Id := fmt.Sprintf("%d", binary.LittleEndian.Uint32(data))
+	id := ParseFromWg26Number(wg26Id)
+	t.Log("Number: ", id.Number)
+	t.Log("Wg26Hex: ", id.Wg26Hex)
+	t.Logf("Wg26Bytes: %X", id.Wg26Bytes)
+	t.Logf("Wg26Start: %d", id.Wg26Start)
+	t.Logf("Wg26End: %d", id.Wg26End)
 	t.Log("Std: ", id.Std())
 }
